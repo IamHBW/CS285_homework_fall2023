e8c620e05d8935101f6e91313b26bd404e047a8f
Bowen <411435461@qq.com>
2025-09-09 14:37:27 +0800

    added shell scripts that train on different envs

---
+++ hw1/run_hopper.sh
@@ -0,0 +1,4 @@
+python cs285/scripts/run_hw1.py \
+--expert_policy_file cs285/policies/experts/Hopper.pkl \
+--env_name Hopper --exp_name dagger_Hopper --n_iter 10 \
+--do_dagger --expert_data cs285/expert_data/expert_data_Hopper-v4.pkl 
